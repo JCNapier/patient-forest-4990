8818d5496beda331e1559451dea5ce2c990625da
John Napier <jcee.napier@gmail.com>
2021-12-14 12:22:43 -0700

    Migrate new tables and join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
 
-universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
-universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+movie_1 = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+movie_2 = universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+
+actor_1 = Actor.create!(name: 'Harrison Ford', age: 78)
+actor_2 = Actor.create!(name: 'Eddie Murphy', age: 60)
+
+movie_1_actors = MovieActors.create!(movie: movie_1, actor: actor_1)
+movie_2_actors = MovieActors.create!(movie: movie_2, actor: actor_2)
